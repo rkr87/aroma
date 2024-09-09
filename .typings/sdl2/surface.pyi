766f3ae82ffb91a1ab4fd3e1f521ced2d55e1d56
Ryan Rowland <ryan.rowland@gmail.com>
2024-09-09 13:19:56 +0100

    chore(typing): update sdl2 typings

--- .typings/sdl2/surface.pyi
+++ .typings/sdl2/surface.pyi
@@ -1,5 +1,11 @@
 from ctypes import Structure
 
+from sdl2 import SDL_Rect
+
 class SDL_Surface(Structure):
     w: int
     h: int
+
+def SDL_CreateRGBSurface(some_int: int, height: int, width: int, another_int: int, rmask: int, gmask: int, bmask: int, amask:int) ->SDL_Surface: ...
+def SDL_UpperBlit(surface: SDL_Surface, rect: SDL_Rect | None, surface2: SDL_Surface, rect2: SDL_Rect) -> int: ...
+SDL_BlitSurface = SDL_UpperBlit
