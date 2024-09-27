40496b71779b3f95e60d7b19dedf99d1ff6fc885
Ryan Rowland <ryan.rowland@gmail.com>
2024-09-27 17:26:05 +0100

    feat(rom_manager): add image scraping calls

--- src/aroma/aroma/manager/rom_manager.py
+++ src/aroma/aroma/manager/rom_manager.py
@@ -1,4 +1,4 @@
-"""TODO."""
+"""Defines the RomManager class for managing ROMs and associated resources."""
 
 from classes.base.class_singleton import ClassSingleton
 from data.rom_db import RomDB
@@ -8,7 +8,7 @@ from model.app_config import AppConfig
 
 
 class RomManager(ClassSingleton):
-    """TODO."""
+    """Handles ROM database operations and related image management."""
 
     def __init__(self) -> None:
         super().__init__()
@@ -17,13 +17,20 @@ class RomManager(ClassSingleton):
         self._images = ImageManager()
 
     def refresh_roms(self) -> None:
-        """Refresh ROMs in app database and update TSP cache dbs."""
+        """Refresh ROMs in the app database and update the TSP cache."""
         self._rom_db.update()
         self._cache.update_cache_db(self._rom_db.data)
         if AppConfig().remove_broken_images_on_refresh:
             self._images.remove_broken_images(self._rom_db.valid_paths)
+        if AppConfig().scrape_on_refresh:
+            self._images.scrape_images(self._rom_db.data)
 
     def remove_broken_images(self) -> None:
         """Remove images not associated with valid ROM paths."""
         self._rom_db.update()
         self._images.remove_broken_images(self._rom_db.valid_paths)
+
+    def scrape_missing_images(self) -> None:
+        """Scrape and download missing images for valid ROMs."""
+        self._rom_db.update()
+        self._images.scrape_images(self._rom_db.data)
