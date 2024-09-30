bc3ab144dac29edc0fffaacb25b734a27df4fe44
Ryan Rowland <ryan.rowland@gmail.com>
2024-09-30 14:22:06 +0100

    feat(text_gen): add text style of deactivated items
    feat(text_gen): add ability to truncate long lines
    feat(text_gen): add ability to truncate multiple lines by max height

--- src/aroma/aroma/app/render/text_generator.py
+++ src/aroma/aroma/app/render/text_generator.py
@@ -3,7 +3,13 @@
 from enum import StrEnum
 
 from classes.base.class_singleton import ClassSingleton
-from constants import PRIMARY_COLOR, RESOURCES, SECONDARY_COLOR
+from constants import (
+    INACTIVE_COLOR,
+    INACTIVE_SEL,
+    PRIMARY_COLOR,
+    RESOURCES,
+    SECONDARY_COLOR,
+)
 from sdl2 import (
     SDL_BlitSurface,
     SDL_CreateRGBSurface,
@@ -25,6 +31,8 @@ class Style(StrEnum):
     BUTTON_HELP_TEXT = "button_help_text"
     SIDEPANE_HEADING = "sidepane_heading"
     SIDEPANE_CONTENT = "sidepane_content"
+    INACTIVE = "deactivated"
+    INACTIVE_SELECTED = "inactive_selected"
 
 
 class TextGenerator(ClassSingleton):
@@ -36,8 +44,12 @@ class TextGenerator(ClassSingleton):
         super().__init__()
         color = tuple_to_sdl_color(PRIMARY_COLOR)
         selected = tuple_to_sdl_color(SECONDARY_COLOR)
+        inactive = tuple_to_sdl_color(INACTIVE_COLOR)
+        inactive_sel = tuple_to_sdl_color(INACTIVE_SEL)
         self.font = ext.FontTTF(font_path or self._DEFAULT_FONT, 26, color)
         self.font.add_style(Style.SELECTED.value, 26, selected)
+        self.font.add_style(Style.INACTIVE.value, 26, inactive)
+        self.font.add_style(Style.INACTIVE_SELECTED.value, 26, inactive_sel)
         self.font.add_style(Style.BOTTOM.value, 20, selected)
         self.font.add_style(Style.BREADCRUMB.value, 20, selected)
         self.font.add_style(Style.BREADCRUMB_TRAIL.value, 20, color)
@@ -51,26 +63,49 @@ class TextGenerator(ClassSingleton):
         text: str,
         *,
         selected: bool,
+        deactivated: bool = False,
+        max_width: int = 0,
     ) -> SDL_Surface | None:
         """Return a selectable text surface."""
-        style: Style = Style.SELECTED if selected else Style.DEFAULT
-        return self.get_text(text, style)
+        if not deactivated:
+            style: Style = Style.SELECTED if selected else Style.DEFAULT
+        else:
+            style = Style.INACTIVE_SELECTED if selected else Style.INACTIVE
+        return self.get_text(text, style, max_width)
 
     def get_text(
-        self,
-        text: str,
-        style: Style = Style.DEFAULT,
+        self, text: str, style: Style = Style.DEFAULT, max_width: int = 0
     ) -> SDL_Surface | None:
         """Render the text using the provided style."""
         if not text:
-            self._logger.warning("Empty text provided for style %s", style)
+            self._logger.debug("Empty text provided for style %s", style)
             return None
-        if (text_surface := self.font.render_text(text, style.value)) is None:
-            self._logger.error(
-                "Failed to render text to surface for style %s",
-                style,
-            )
-        return text_surface
+
+        if not (text_surface := self._render_surface(text, style)):
+            self._logger.error("Failed to render text for style %s", style)
+            return None
+
+        if not max_width or text_surface.w < max_width:
+            return text_surface
+
+        return self._wrap_text_to_width(text, style, max_width)
+
+    def _render_surface(self, text: str, style: Style) -> SDL_Surface | None:
+        """Render text surface with the specified style."""
+        return self.font.render_text(text, style.value)
+
+    def _wrap_text_to_width(
+        self, text: str, style: Style, max_width: int
+    ) -> SDL_Surface | None:
+        """Wrap text to fit within the specified width."""
+        new_text = ""
+        for word in text.split(" "):
+            test_line = f"{new_text} {word}".strip()
+            text_surface = self._render_surface(test_line, style)
+            if text_surface and text_surface.w > max_width:
+                return self._render_surface(f"{new_text}...", style)
+            new_text = test_line
+        return self._render_surface(new_text, style)
 
     @staticmethod
     def _create_fixed_surface(width: int, height: int) -> SDL_Surface:
@@ -142,24 +177,11 @@ class TextGenerator(ClassSingleton):
         )
         return wrapped_lines
 
-    def get_wrapped_text(
-        self,
-        text: str,
-        max_width: int,
-        style: Style,
-    ) -> SDL_Surface | None:
-        """Wrap text within the given width."""
-        if not (lines := self._get_wrapped_lines(text, max_width, style)):
-            self._logger.warning(
-                "No text surfaces created for the given width %d and style %s",
-                max_width,
-                style,
-            )
-            return None
-
-        total_height = sum(surface.h for surface in lines)
-        final_surface = self._create_fixed_surface(max_width, total_height)
-
+    @staticmethod
+    def _blit_lines_to_surface(
+        final_surface: SDL_Surface, lines: list[SDL_Surface]
+    ) -> None:
+        """Blit the lines onto the final surface."""
         y_offset = 0
         for surface in lines:
             SDL_BlitSurface(
@@ -169,6 +191,45 @@ class TextGenerator(ClassSingleton):
                 SDL_Rect(0, y_offset, surface.w, surface.h),
             )
             y_offset += surface.h
+
+    @staticmethod
+    def _truncate_lines(
+        lines: list[SDL_Surface], max_height: int
+    ) -> tuple[list[SDL_Surface], int]:
+        """Truncate the lines to fit within the maximum height."""
+        truncated_lines: list[SDL_Surface] = []
+        truncated_height = 0
+        for surface in lines:
+            if truncated_height + surface.h > max_height:
+                break
+            truncated_lines.append(surface)
+            truncated_height += surface.h
+        return truncated_lines, truncated_height
+
+    def get_wrapped_text(
+        self,
+        text: str,
+        max_width: int,
+        style: Style,
+        max_height: int | None = None,
+    ) -> SDL_Surface | None:
+        """Wrap text within the given width and optional maximum height."""
+        if not (lines := self._get_wrapped_lines(text, max_width, style)):
+            self._logger.info(
+                "No text surfaces created for the given width %d and style %s",
+                max_width,
+                style,
+            )
+            return None
+
+        total_height = sum(surface.h for surface in lines)
+
+        if max_height is not None and total_height > max_height:
+            lines, total_height = self._truncate_lines(lines, max_height)
+
+        final_surface = self._create_fixed_surface(max_width, total_height)
+
+        self._blit_lines_to_surface(final_surface, lines)
 
         self._logger.debug(
             "Final wrapped text surface created with size %dx%d",
