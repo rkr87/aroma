0bc89aee5fb3553c1e9eaac0098b1084be69a74e
Ryan Rowland <ryan.rowland@gmail.com>
2024-10-06 10:35:39 +0100

    feat(project): replace emu validations with emu manager/config calls

--- src/aroma/aroma/data/validator/rom_validator.py
+++ src/aroma/aroma/data/validator/rom_validator.py
@@ -4,30 +4,16 @@ from pathlib import Path
 
 from classes.base.class_singleton import ClassSingleton
 from constants import (
-    EMU_EXT_KEY,
-    EMU_PATH,
     ROM_DB_IGNORE_EXT,
     ROM_DB_IGNORE_WORDS,
     ROM_PATH,
 )
-from tools import util
+from data.emu_config_handler import EmuConfigHandler
+from manager.emu_manager import EmuManager
 
 
 class RomValidator(ClassSingleton):
     """Validates ROM files based on various criteria."""
-
-    def __init__(self) -> None:
-        super().__init__()
-        self._system_exts: dict[str, list[str]] = {}
-
-    @staticmethod
-    def _get_valid_emu_ext(system: str) -> list[str]:
-        """Return valid emulator extensions for provided system."""
-        emu_config = EMU_PATH / system / "config.json"
-        data = util.load_simple_json(emu_config)
-        if EMU_EXT_KEY not in data or not data[EMU_EXT_KEY]:
-            return []
-        return [ext.lower() for ext in data[EMU_EXT_KEY].split("|")]
 
     @staticmethod
     def _is_not_hidden(path: Path) -> bool:
@@ -54,28 +40,30 @@ class RomValidator(ClassSingleton):
     @staticmethod
     def _has_valid_system_directory(path: Path) -> bool:
         """Check if the emu directory exists for the ROM."""
-        relative_path = path.relative_to(ROM_PATH)
-        system = relative_path.parts[0]
-        return (EMU_PATH / system).is_dir()
+        system = path.relative_to(ROM_PATH).parts[0]
+        return EmuManager.is_valid_system(system)
 
-    def has_valid_ext(self, path: Path) -> bool:
+    @staticmethod
+    def has_valid_ext(path: Path) -> bool:
         """Validate the file extension against the allowed extensions."""
-        relative_path = path.relative_to(ROM_PATH)
-        if (system := relative_path.parts[0]) not in self._system_exts:
-            self._system_exts[system] = self._get_valid_emu_ext(system)
         ext = path.suffix.lstrip(".").lower()
-        return not (exts := self._system_exts[system]) or ext in exts
+        system = path.relative_to(ROM_PATH).parts[0]
+        return (
+            not (exts := EmuConfigHandler().get(system).valid_ext)
+            or ext in exts
+        )
 
-    def check_path(self, path: Path) -> bool:
+    @staticmethod
+    def check_path(path: Path) -> bool:
         """Validate the entire path based on multiple criteria."""
         if not path.is_file():
             return False
         checks = [
-            self._is_not_hidden,
-            self._is_not_ignored_ext,
-            self._is_not_ignored_word,
-            self._has_valid_relative_path,
-            self._has_valid_system_directory,
-            self.has_valid_ext,
+            RomValidator._is_not_hidden,
+            RomValidator._is_not_ignored_ext,
+            RomValidator._is_not_ignored_word,
+            RomValidator._has_valid_relative_path,
+            RomValidator._has_valid_system_directory,
+            RomValidator.has_valid_ext,
         ]
         return all(check(path) for check in checks)
