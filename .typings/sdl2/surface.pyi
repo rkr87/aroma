fcaa0efc691e04a517f10ca9900bed40a865e193
Ryan Rowland <ryan.rowland@gmail.com>
2024-10-13 12:58:07 +0100

    chore(typings): update sdl2 typing

--- .typings/sdl2/surface.pyi
+++ .typings/sdl2/surface.pyi
@@ -6,6 +6,33 @@ class SDL_Surface(Structure):
     w: int
     h: int
 
-def SDL_CreateRGBSurface(some_int: int, height: int, width: int, another_int: int, rmask: int, gmask: int, bmask: int, amask:int) ->SDL_Surface: ...
-def SDL_UpperBlit(surface: SDL_Surface, rect: SDL_Rect | None, surface2: SDL_Surface, rect2: SDL_Rect) -> int: ...
+def SDL_CreateRGBSurface(
+    some_int: int,
+    height: int,
+    width: int,
+    another_int: int,
+    rmask: int,
+    gmask: int,
+    bmask: int,
+    amask: int,
+) -> SDL_Surface: ...
+def SDL_UpperBlit(
+    surface: SDL_Surface,
+    rect: SDL_Rect | None,
+    surface2: SDL_Surface,
+    rect2: SDL_Rect | None,
+) -> int: ...
+def SDL_CreateRGBSurfaceWithFormat(
+    huh: int, width: int, height: int, another: int, fmt: int
+) -> SDL_Surface: ...
+def SDL_SetSurfaceBlendMode(surface: SDL_Surface, blend_mode: int) -> int: ...
+def SDL_BlitScaled(
+    surface: SDL_Surface,
+    rect: SDL_Rect | None,
+    surface2: SDL_Surface,
+    rect2: SDL_Rect | None,
+) -> int: ...
+
 SDL_BlitSurface = SDL_UpperBlit
+
+def SDL_FreeSurface(surface: SDL_Surface) -> None: ...
