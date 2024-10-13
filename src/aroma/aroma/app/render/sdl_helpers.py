4a13247d694e1f1d685651ef11afb8cc07fe577c
Ryan Rowland <ryan.rowland@gmail.com>
2024-10-13 13:24:32 +0100

    feat(screen): split sdl helpers into separate class

--- src/aroma/aroma/app/render/sdl_helpers.py
+++ src/aroma/aroma/app/render/sdl_helpers.py
@@ -1,23 +1,50 @@
 """Handles screen rendering for the application."""
 
 import ctypes
+from pathlib import Path
 
 from sdl2 import (
+    SDL_BLENDMODE_BLEND,
+    SDL_PIXELFORMAT_RGBA32,
+    SDL_BlitScaled,
+    SDL_CreateRGBSurface,
+    SDL_CreateRGBSurfaceWithFormat,
     SDL_CreateTextureFromSurface,
     SDL_DestroyTexture,
+    SDL_FreeSurface,
     SDL_Rect,
     SDL_RenderCopy,
     SDL_RenderDrawLine,
     SDL_SetRenderDrawColor,
+    SDL_SetSurfaceBlendMode,
     SDL_Surface,
     SDL_Texture,
 )
-from sdl2.ext import Color, Renderer
+from sdl2.ext import Color, Renderer, load_image
 from shared.classes.class_singleton import ClassSingleton
 
 
 class SDLHelpers(ClassSingleton):
     """TODO."""
+
+    @staticmethod
+    def create_fixed_surface(width: int, height: int) -> SDL_Surface:
+        """Create a fixed-size SDL_Surface."""
+        rmask = 0x000000FF
+        gmask = 0x0000FF00
+        bmask = 0x00FF0000
+        amask = 0xFF000000
+
+        return SDL_CreateRGBSurface(
+            0,
+            width,
+            height,
+            32,
+            rmask,
+            gmask,
+            bmask,
+            amask,
+        )
 
     @staticmethod
     def draw_line(
@@ -67,6 +94,7 @@ class SDLHelpers(ClassSingleton):
             SDLHelpers.get_static_logger().error(
                 "Failed to create texture from surface"
             )
+            SDL_FreeSurface(surface)
             return
 
         if isinstance(surface, ctypes.POINTER(SDL_Surface)):
@@ -76,9 +104,47 @@ class SDLHelpers(ClassSingleton):
             surface_width = surface.w
             surface_height = surface.h
 
+        SDL_FreeSurface(surface)
         dstrect = SDL_Rect(x, y, surface_width, surface_height)
         SDL_RenderCopy(renderer.sdlrenderer, texture, None, dstrect)
         SDL_DestroyTexture(texture)
         SDLHelpers.get_static_logger().debug(
             "Rendered surface at (%d, %d)", x, y
         )
+
+    @staticmethod
+    def _scale_image(
+        image_path: str, max_width: int, max_height: int
+    ) -> SDL_Surface | None:
+        """TODO."""
+        if not (surface := load_image(image_path)):
+            return None
+        scale_factor = min(max_width / surface.w, max_height / surface.h, 1)
+        new_width = int(surface.w * scale_factor)
+        new_height = int(surface.h * scale_factor)
+        has_alpha = surface.format.contents.Amask != 0
+        scaled_surface = SDL_CreateRGBSurfaceWithFormat(
+            0,
+            new_width,
+            new_height,
+            32,
+            SDL_PIXELFORMAT_RGBA32,
+        )
+        if not scaled_surface:
+            SDL_FreeSurface(surface)
+            return None
+        if has_alpha:
+            SDL_SetSurfaceBlendMode(scaled_surface, SDL_BLENDMODE_BLEND)
+
+        SDL_BlitScaled(surface, None, scaled_surface, None)
+        SDL_FreeSurface(surface)
+        return scaled_surface
+
+    @staticmethod
+    def load_scaled_image(
+        image_path: str | None, max_width: int, max_height: int
+    ) -> SDL_Surface | None:
+        """TODO."""
+        if not image_path or not Path(image_path).is_file():
+            return None
+        return SDLHelpers._scale_image(image_path, max_width, max_height)
