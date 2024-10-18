f6ea6fae67289725ebdaeed0753d0facbc9ace27
Ryan Rowland <ryan.rowland@gmail.com>
2024-10-18 10:37:47 +0100

    feat(screen): add screen_manager

---
+++ src/aroma/aroma/app/screen_manager.py
@@ -0,0 +1,63 @@
+"""Handles screen rendering for the application."""
+
+from app.model.current_menu import CurrentMenu
+from app.render.breadcrumb_renderer import BreadcrumbRenderer
+from app.render.button_hint_renderer import ButtonHintRenderer
+from app.render.menu_renderer import MenuRenderer
+from app.render.sidepane_renderer import SidepaneRenderer
+from app.render.work_overlay_renderer import WorkOverlayRenderer
+from sdl2.ext import Renderer, Window
+from shared.classes.class_singleton import ClassSingleton
+from shared.constants import (
+    APP_NAME,
+    BG_COLOR,
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+)
+from shared.tools.util import tuple_to_sdl_color
+
+_SPACING = 13
+_PADDING = 39
+_SPLIT_PANE = int((SCREEN_WIDTH / 7 * 3) + _PADDING + _SPACING)
+
+
+class ScreenManager(ClassSingleton):
+    """Manages the screen rendering process."""
+
+    def __init__(self) -> None:
+        super().__init__()
+        window = Window(APP_NAME, size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+        window.show()
+        self.renderer = Renderer(window)
+        self.sidepane = SidepaneRenderer(
+            self.renderer, _SPLIT_PANE, _PADDING, _SPACING
+        )
+        self.menu = MenuRenderer(
+            self.renderer, _SPLIT_PANE, _PADDING, _SPACING
+        )
+        self._logger.info("Screen initialised")
+
+    def render(self, current: CurrentMenu) -> None:
+        """Render the screen background, breadcrumbs, menu, and side pane."""
+        if current.update_required:
+            self.renderer.clear(tuple_to_sdl_color(BG_COLOR))
+            button_hint_height = ButtonHintRenderer.render(
+                self.renderer, _SPACING
+            )
+            breadcrumb_height = BreadcrumbRenderer.render(
+                self.renderer, current.breadcrumbs, _SPACING
+            )
+            self.menu.render(
+                current.menu,
+                breadcrumb_height + _SPACING,
+                _SPLIT_PANE - _SPACING,
+            )
+            self.sidepane.render(
+                current.menu.content.side_pane,
+                breadcrumb_height + _SPACING,
+                button_hint_height,
+            )
+            WorkOverlayRenderer.render(self.renderer)
+            self.renderer.present()
+            current.update_required = False
+            self._logger.debug("Rendered screen for current menu")
