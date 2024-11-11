bb4603ecb2f569d37b19deb98ba419cb24f9144d
GitHub Action <action@github.com>
2024-11-11 10:02:25 +0000

    chore(ci): bump 0.4.0a7 -> 0.4.0a8 [skip ci]

--- src/aroma/aroma/updater.py
+++ src/aroma/aroma/updater.py
@@ -12,7 +12,7 @@ _REPO_PATH = "rkr87/aroma/releases/latest"
 _LATEST_RELEASE = f"https://api.github.com/repos/{_REPO_PATH}"
 _ASSET = f"https://github.com/{_REPO_PATH}/download/aroma.zip"
 
-VERSION = "0.4.0a7"
+VERSION = "0.4.0a8"
 
 
 class Updater(ClassSingleton):
